cfd3596233a05c21c2abf333c572621314fd500a
rein14 <reinrich14@gmail.com>
2018-10-10 04:36:05 +0000

    all

--- market/settings.py
+++ market/settings.py
@@ -183,7 +183,7 @@ WSGI_APPLICATION = 'market.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -195,8 +195,12 @@ DATABASES = {
         'ATOMIC_REQUESTS': True,
     }
 }
-
-
+'''
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
