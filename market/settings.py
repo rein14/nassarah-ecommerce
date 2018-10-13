fea6601ff2a33c1424f5a6bf8dd44c0c547f406b
rein14 <reinrich14@gmail.com>
2018-10-13 05:22:02 +0000

    all files

--- market/settings.py
+++ market/settings.py
@@ -248,14 +248,14 @@ USE_L10N = True
 def location(x): return os.path.join(
     os.path.dirname(os.path.realpath(__file__)), x)
 
-AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = 'AKIAIXJ6TVKSOC6ABAPA'
 AWS_S3_ENCRYPTION = True
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'nasara'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
-} 
+}
 AWS_LOCATION = 'static'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
